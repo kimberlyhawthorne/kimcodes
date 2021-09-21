65048f1d8abfb7a447657c32bec1b17463aaf1f6
Kimberly Hawthorne <kimberlyrhawthorne@gmail.com>
2021-09-21 19:26:21 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Kim Hawthorne",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
