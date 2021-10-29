5bbbc27de60a2f796c02373b781f2b2e229345e4
Kimberly Hawthorne <kimberlyrhawthorne@gmail.com>
2021-10-29 14:43:29 -0400

    fix heading line heights

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -4,20 +4,24 @@ import {FONTS, COLORS} from 'styles/variables';
 export const Heading1 = styled.h1`
 	font-family: ${FONTS.bold};
 	color: ${COLORS.lilac};
+	line-height: normal;
 `;
 
 export const Heading2 = styled.h2`
 	font-family: ${FONTS.bold};
 	color: ${COLORS.lilac};
+	line-height: normal;
 `;
 
 export const Heading3 = styled.h3`
 	font-family: ${FONTS.bold};
 	color: ${COLORS.lilac};
+	line-height: normal;
 `;
 
 export const Heading4 = styled.h4`
 	font-family: ${FONTS.bold};
+	line-height: normal;
 `;
 
 export const SectionTitle = styled(Heading2)`
@@ -29,7 +33,6 @@ export const SectionTitle = styled(Heading2)`
 export const BodyText = `
 	font-family: ${FONTS.regular};
 	font-size: 1rem;
-	line-height: 1.25rem;
 `;
 
 // shared styles for all text links
